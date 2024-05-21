bdbb39b75ea2f1d5defec11619d46a79e63d8df0
Roni-Angress <138021578+Roni-Angress@users.noreply.github.com>
2024-05-21 18:58:40 +0300

    utilized score.py and created main_score.py

--- games/score.py
+++ games/score.py
@@ -1,25 +1,32 @@
 import os
 import utils
-path = utils.SCORES_FILE_NAME
+
+scores_file_path = utils.SCORES_FILE_NAME
 
 def add_score(difficulty: int):
-    """The function’s input is a variable called difficulty. The function will try to read
-    the current score in the scores file, if it fails it will create a new one and will use it to save the
-    current score."""
+    """
+    Adds the score based on the difficulty level to the scores file.
+
+    Parameters:
+    difficulty (int): The difficulty level to determine the points to be added.
+    """
     points_of_winning = (difficulty * 3) + 5
+
+    # Initialize the current score
     current_score = 0
 
-    if os.path.isfile(path):
-        with open(path, 'r') as scores_file:
-            file_content = scores_file.readlines()
-            current_score = int(file_content[0])
+    if os.path.isfile(scores_file_path):
+        with open(scores_file_path, 'r') as scores_file:
+            # Read the first line and attempt to parse the score
+            first_line = scores_file.readline().strip()
+            if first_line.isdigit():
+                current_score = int(first_line)
+            else:
+                print(f"Warning: Invalid score found in {scores_file_path}. Starting from 0.")
 
-    else:
-        with open(path, 'w') as scores_file:
-            scores_file.write("")
-
-    # now add the new score to the file
-
+    # Calculate the new score
     new_score = current_score + points_of_winning
-    with open(path, 'w') as scores_file:
+
+    # Write the new score back to the file. If the file does not exist it will be created regardless.
+    with open(scores_file_path, 'w') as scores_file:
         scores_file.write(str(new_score))
