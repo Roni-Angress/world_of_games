ecbf3762af83342c66b1be0e41c02279d58d68f9
Roni-Angress <138021578+Roni-Angress@users.noreply.github.com>
2024-05-21 20:13:59 +0300

    Utilized main_score.py to display the score on a local HTTP server

--- games/main_score.py
+++ games/main_score.py
@@ -1,10 +1,16 @@
-import flask
+from flask import Flask, render_template
+import games.score as score
 import utils
 """This file’s sole purpose is to serve the user’s score currently in the scores.txt file over HTTP with HTML.
 This will be done by using python’s flask library."""
-
 scores_file_path = utils.SCORES_FILE_NAME
 
+app = Flask(__name__)
+
+@app.route("/")
 def score_server():
-    """This function will serve the score. It will read the score from the scores file and
-        will return an HTML"""
+    current_score = str(score.get_score())
+    return render_template('score.html', SCORE=current_score)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- games/score.py
+++ games/score.py
@@ -3,6 +3,25 @@ import utils
 
 scores_file_path = utils.SCORES_FILE_NAME
 
+
+def get_score():
+    """
+    This function will serve the score
+    """
+    # Initialize the current score
+    current_score = 0
+    # Check if the scores file exists and read the score
+    if os.path.isfile(scores_file_path):
+        with open(scores_file_path, 'r') as scores_file:
+            # Read the first line and attempt to parse the score
+            first_line = scores_file.readline().strip()
+            if first_line.isdigit():
+                current_score = int(first_line)
+            else:
+                print(f"Warning: Invalid score found in {scores_file_path}. Starting from 0.")
+    return current_score
+
+
 def add_score(difficulty: int):
     """
     Adds the score based on the difficulty level to the scores file.
@@ -13,17 +32,7 @@ def add_score(difficulty: int):
     points_of_winning = (difficulty * 3) + 5
 
     # Initialize the current score
-    current_score = 0
-
-    if os.path.isfile(scores_file_path):
-        with open(scores_file_path, 'r') as scores_file:
-            # Read the first line and attempt to parse the score
-            first_line = scores_file.readline().strip()
-            if first_line.isdigit():
-                current_score = int(first_line)
-            else:
-                print(f"Warning: Invalid score found in {scores_file_path}. Starting from 0.")
-
+    current_score = get_score()
     # Calculate the new score
     new_score = current_score + points_of_winning
 
